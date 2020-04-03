031a0f72377652db5edef43e554ce05137a8d333
arminv <arminv@users.noreply.github.com>
2020-04-03 00:33:02 -0400

    End of lesson 41

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        {/* NOTE: Switch stops the page from always hitting the redirect and stops as soon as a route is hit: */}
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
