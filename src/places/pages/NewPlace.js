35fb414d4c2080c56c3d1c9e4dbf3b6e46311aa2
arminv <arminv@users.noreply.github.com>
2020-05-04 21:50:09 -0400

    End of lesson 61

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,19 +1,67 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from '../../shared/util/validators';
 import './NewPlace.css';
 
+const fromReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+  const [formState, dispatch] = useReducer(fromReducer, {
+    title: {
+      value: '',
+      isValid: false,
+    },
+    description: {
+      value: '',
+      isValid: false,
+    },
+    isValid: false,
+  });
   // NOTE: if we don't use 'useCallback', we risk having and infinite loop because of the 'useEffect' in Input.js:
-  const titleChangeHandler = useCallback((id, value, isValid) => {}, []);
-  const descriptionChangeHandler = useCallback((id, value, isValid) => {}, []);
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+    // dispatch can be a dependancy but react automatically ensures it is not changed so we can just leave it out:
+  }, []);
+
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs); //send this to the backend later
+  };
 
   return (
-    <form className='place-form'>
+    <form className='place-form' onSubmit={placeSubmitHandler}>
       <Input
         id='title'
         element='input'
@@ -21,7 +69,7 @@ const NewPlace = () => {
         label='Title'
         validators={[VALIDATOR_REQUIRE()]}
         errorText='Please input a valid title.'
-        onInput={titleChangeHandler}
+        onInput={inputHandler}
       />
       <Input
         id='description'
@@ -29,8 +77,19 @@ const NewPlace = () => {
         label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText='Please input a valid description (at least 5 characters).'
-        onInput={descriptionChangeHandler}
+        onInput={inputHandler}
       />
+      <Input
+        id='address'
+        element='input'
+        label='Address'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please input a valid address.'
+        onInput={inputHandler}
+      />
+      <Button type='submit' disabled={!formState.isValid}>
+        Add Place
+      </Button>
     </form>
   );
 };
