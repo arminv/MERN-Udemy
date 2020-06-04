ae18c76b12987ee274aa7acd7e1b61ead51d3b1f
arminv <arminv@users.noreply.github.com>
2020-06-03 21:52:20 -0400

    End of lesson 69

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from './user/pages/Auth';
 
 const App = () => {
   return (
@@ -30,6 +31,9 @@ const App = () => {
           {/* If this route was placed before /places/new, we would have a problem as new would be caught as a variable: */}
           <Route path='/places/:placeId'>
             <UpdatePlace />
+          </Route>
+          <Route path='/auth'>
+            <Auth />
           </Route>
           <Redirect to='/' />
         </Switch>
