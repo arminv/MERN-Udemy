6e7bc7800d337aca0404f2e529866fbc5f62d212
arminv <arminv@users.noreply.github.com>
2020-06-05 21:01:03 -0400

    End of lesson 76 (Section 5)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,33 +12,66 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+        {/* If this route was placed before /places/new, we would have a problem as new would be caught as a variable: */}
+        <Route path='/places/:placeId'>
+          <UpdatePlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
+        <Route path='/auth'>
+          <Auth />
+        </Route>
+        <Redirect to='/auth' />
+      </Switch>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Users />
-          </Route>
-          <Route path='/:userId/places' exact>
-            <UserPlaces />
-          </Route>
-          <Route path='/places/new' exact>
-            <NewPlace />
-          </Route>
-          {/* If this route was placed before /places/new, we would have a problem as new would be caught as a variable: */}
-          <Route path='/places/:placeId'>
-            <UpdatePlace />
-          </Route>
-          <Route path='/auth'>
-            <Auth />
-          </Route>
-          <Redirect to='/' />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
